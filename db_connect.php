3513fdadf77eb31bede1d704ec024285c14117ab
amin <aminsum9@gmail.com>
2021-01-26 20:23:06 +0700

    a

--- db_connect.php
+++ db_connect.php
@@ -2,10 +2,18 @@
 
 // $db_conn = mysqli_connect('localhost', 'root', '', 'warung_pintar');
 
-$db_host        = '127.0.0.1';
-$db_user        = 'root';
-$db_pass        = '';
-$db_database    = 'warung_pintar'; 
+//local
+// $db_host        = '127.0.0.1';
+// $db_user        = 'root';
+// $db_pass        = '';
+// $db_database    = 'warung_pintar'; 
+// $db_port        = '3306';
+
+//remote
+$db_host        = 'remotemysql.com';
+$db_user        = 'rqVWI5GSIR';
+$db_pass        = 'xyNrCK4VzR';
+$db_database    = 'rqVWI5GSIR';
 $db_port        = '3306';
 
 $db_conn = mysqli_connect($db_host,$db_user,$db_pass,$db_database,$db_port);
